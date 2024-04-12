2e53ef7b6389c355c28ac8e287a73eaf1ee500bf
jimmase <jimiama.mafenimase2@nottingham.ac.uk>
2024-04-12 13:28:16 +0100

    feature importance options created

---
+++ feature_importance/call_methods.py
@@ -0,0 +1,8 @@
+import argparse
+
+
+def load_models(opt: argparse.Namespace):    
+    raise NotImplementedError(f"Funtion load_models is not implemented")
+
+def load_data(opt: argparse.Namespace):   
+    raise NotImplementedError(f"Funtion load_data is not implemented")
--- feature_importance/fi_options.py
+++ feature_importance/fi_options.py
@@ -1,1 +1,58 @@
 
+
+import os
+import sys
+import ast
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from base_options import BaseOptions
+
+
+
+
+class FIOptions(BaseOptions):
+    """
+    Feature Importance options
+    Parent class: BaseOptions
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def initialize(self) -> None:
+        """Initialize train options"""
+        BaseOptions.initialize(self)
+
+        self.parser.add_argument(
+            "--fi_methods",
+            type=lambda x: ast.literal_eval(x),
+            default={'Permutation Importance': False,
+                     'SHAP': False,},
+            help="Feature importance methods to use",
+        ),
+
+        self.parser.add_argument(
+            "--random_state",
+            type=int,
+            default=42,
+            help="Random state for reproducibility",
+        ),
+
+        self.parser.add_argument(
+            "--save_fi_results",
+            type=bool,
+            default=True,
+            help="Flag to save feature importance results",
+        ),
+
+        self.parser.add_argument(
+            "--save_fi_plots",
+            type=bool,
+            default=True,
+            help="Flag to save feature importance plots",
+        ),
+
+        # Update --is_feature_importance to True
+        self.parser.set_defaults(is_feature_importance=True),
