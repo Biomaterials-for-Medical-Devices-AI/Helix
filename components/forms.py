6026242676eb59f5fac075af1f27edf826531354
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-10-22 11:49:43 +0100

    directory input component

--- components/forms.py
+++ components/forms.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 import streamlit as st
 
 from options.enums import ConfigStateKeys, ExecutionStateKeys
@@ -5,6 +7,7 @@ from options.enums import ConfigStateKeys, ExecutionStateKeys
 
 def data_upload_form():
     st.header("Data Upload")
+    save_dir = _save_directory_selector()
     st.text_input("Name of the experiment", key=ConfigStateKeys.ExperimentName)
     st.text_input(
         "Name of the dependent variable", key=ConfigStateKeys.DependentVariableName
@@ -20,3 +23,15 @@ def data_upload_form():
             key=ConfigStateKeys.UploadedModels,
         )
     st.button("Run", key=ExecutionStateKeys.RunPipeline)
+
+
+def _save_directory_selector() -> Path:
+    """Create a selector for the directory to save experiments."""
+    root = Path.home()
+
+    col1, col2 = st.columns([0.3, 0.7], vertical_alignment="bottom")
+
+    col1.text(f"{root}/")
+    sub_dir = col2.text_input(label="", placeholder="Directory name")
+
+    return root / sub_dir
