3acb486da637def421232ae39645883ef1d08796
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-02-18 13:51:56 +0000

    DataSplitOptions class

--- biofefi/options/data.py
+++ biofefi/options/data.py
@@ -4,8 +4,15 @@ from biofefi.options.enums import Normalisations
 
 
 @dataclass
+class DataSplitOptions:
+    n_bootstraps: int | None = None
+    k_folds: int | None = None
+    test_size: float = 0.2
+
+
+@dataclass
 class DataOptions:
     data_path: str | None = None
-    data_split: dict | None = None
+    data_split: DataSplitOptions | None = None
     dependent_variable: str | None = None
     normalisation: Normalisations = Normalisations.NoNormalisation
