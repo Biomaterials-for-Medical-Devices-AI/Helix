fce9ab7bbf3ed0faf853e247954bbed396412cd4
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-02-18 14:09:48 +0000

    docstring

--- biofefi/options/data.py
+++ biofefi/options/data.py
@@ -5,6 +5,11 @@ from biofefi.options.enums import Normalisations
 
 @dataclass
 class DataSplitOptions:
+    """
+    Options class specifying how to split data in bootstrapping or cross-validation,
+    as well as the size of the test size.
+    """
+
     n_bootstraps: int | None = None
     k_folds: int | None = None
     test_size: float = 0.2
@@ -12,7 +17,11 @@ class DataSplitOptions:
 
 @dataclass
 class DataOptions:
+    """
+    Options class specifying where data are saved, how to split them, and which
+    normalisation technique to use.
+    """
+
     data_path: str | None = None
     data_split: DataSplitOptions | None = None
-    dependent_variable: str | None = None
     normalisation: Normalisations = Normalisations.NoNormalisation
