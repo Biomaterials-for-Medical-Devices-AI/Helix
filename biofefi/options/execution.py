2cd3f0c463c4d66733731fed51911f7abfd8d686
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-02-19 15:21:48 +0000

    remove data_path from execution options

--- biofefi/options/execution.py
+++ biofefi/options/execution.py
@@ -5,7 +5,6 @@ from biofefi.options.enums import ProblemTypes
 
 @dataclass
 class ExecutionOptions:
-    data_path: str | None = None
     experiment_name: str = "test"
     random_state: int = 1221
     problem_type: ProblemTypes = ProblemTypes.Classification
