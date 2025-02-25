c500a9d6970e6a380925f78d99e44fc28af2c29f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-02-25 09:57:13 +0000

    init test suite

---
+++ tests/pages/test_1_New_Experiment.py
@@ -0,0 +1,14 @@
+# 1. Test no exceptin occurs loading the page
+# 2. Test an experiment is created at the given directory
+# 3. Check the execution options exist and have the expected values
+# 4. Check the plotting options exist and have the expected values
+# 5. Check the data has been uploaded to the experiment
+
+
+def test_page_loads_without_error():
+    # Arrange
+
+    # Act
+
+    # Assert
+    assert False
