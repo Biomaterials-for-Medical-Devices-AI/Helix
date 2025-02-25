e1af1d7391a6955a364c28dada620a4e1b2ff90e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-02-25 10:58:12 +0000

    first integration test

--- tests/pages/test_1_New_Experiment.py
+++ tests/pages/test_1_New_Experiment.py
@@ -4,11 +4,16 @@
 # 4. Check the plotting options exist and have the expected values
 # 5. Check the data has been uploaded to the experiment
 
+from streamlit.testing.v1 import AppTest
+
 
 def test_page_loads_without_error():
     # Arrange
+    at = AppTest.from_file("helix/pages/1_New_Experiment.py")
 
     # Act
+    at.run()
 
     # Assert
-    assert False
+    assert not at.exception
+    assert not at.error
