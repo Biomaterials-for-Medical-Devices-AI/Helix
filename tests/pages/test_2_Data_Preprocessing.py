e09bb8fc20cf3a8a8956264ffab8100a041ccca6
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-02-26 13:22:47 +0000

    test old exp warning

--- tests/pages/test_2_Data_Preprocessing.py
+++ tests/pages/test_2_Data_Preprocessing.py
@@ -64,6 +64,23 @@ def data_opts(execution_opts: ExecutionOptions):
 
 
 @pytest.fixture
+def old_preproc_opts():
+    return PreprocessingOptions(
+        feature_selection_methods={
+            DataPreprocessingStateKeys.VarianceThreshold: True,
+            DataPreprocessingStateKeys.CorrelationThreshold: True,
+            DataPreprocessingStateKeys.LassoFeatureSelection: True,
+        },
+        variance_threshold=0.10,
+        correlation_threshold=0.80,
+        lasso_regularisation_term=0.05,
+        independent_variable_normalisation=NORMALISATIONS[0].lower(),
+        dependent_variable_transformation=NORMALISATIONS[-1].lower(),
+        data_is_preprocessed=True,
+    )
+
+
+@pytest.fixture
 def dummy_data(execution_opts: ExecutionOptions):
     X, y = make_classification(
         n_samples=5000,
@@ -94,6 +111,38 @@ def new_experiment(
 
     data_opts_file_path = data_options_path(experiment_dir)
     save_options(data_opts_file_path, data_opts)
+
+    np.savetxt(data_opts.data_path, X=dummy_data, delimiter=",")
+
+    yield execution_opts.experiment_name
+
+    if experiment_dir.exists():
+        delete_directory(experiment_dir)
+
+
+@pytest.fixture
+def old_experiment(
+    execution_opts: ExecutionOptions,
+    plotting_opts: PlottingOptions,
+    data_opts: DataOptions,
+    dummy_data: np.ndarray,
+    old_preproc_opts: PreprocessingOptions,
+):
+    base_dir = biofefi_experiments_base_dir()
+    experiment_dir = base_dir / execution_opts.experiment_name
+    create_directory(experiment_dir)
+
+    exec_opts_file_path = execution_options_path(experiment_dir)
+    save_options(exec_opts_file_path, execution_opts)
+
+    plot_opts_file_path = plot_options_path(experiment_dir)
+    save_options(plot_opts_file_path, plotting_opts)
+
+    data_opts_file_path = data_options_path(experiment_dir)
+    save_options(data_opts_file_path, data_opts)
+
+    preproc_file_path = data_preprocessing_options_path(experiment_dir)
+    save_options(preproc_file_path, old_preproc_opts)
 
     np.savetxt(data_opts.data_path, X=dummy_data, delimiter=",")
 
@@ -209,3 +258,28 @@ def test_page_produces_preprocessing_options_file(
     assert expected_file.exists()
     actual_opts = load_data_preprocessing_options(expected_file)
     assert actual_opts == expected_opts
+
+
+def test_page_detects_old_opts(
+    old_experiment: str,
+):
+    # Arrange
+    at = AppTest.from_file("helix/pages/2_Data_Preprocessing.py")
+    at.run()
+
+    # Act
+    # select the experiment
+    at.selectbox[0].select(old_experiment).run()
+    # look for the redo elements
+    redo_warning = at.warning[0]
+    redo_checkbox = at.checkbox[0]
+
+    # Assert
+    assert not at.exception
+    assert not at.error
+    # look
+    assert redo_checkbox.label == "Redo preprocessing"
+    assert (
+        redo_warning.value
+        == "Your data are already preprocessed. Would you like to start again?"
+    )
