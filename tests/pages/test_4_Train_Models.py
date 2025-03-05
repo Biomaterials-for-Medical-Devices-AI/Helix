e54f0837b5280f1ecbb009684b6a9a22f4d3e4f5
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-03-05 13:29:24 +0000

    tidy up

--- tests/pages/test_4_Train_Models.py
+++ tests/pages/test_4_Train_Models.py
@@ -1,12 +1,3 @@
-# 3. b) Test manual Random Forest
-# 3. c) Test manual SVM
-# 3. d) Test manual XGBoost
-# 4. a) Test AHPS Linear Model
-# 4. b) Test AHPS Random Forest
-# 4. c) Test AHPS SVM
-# 4. d) Test AHPS XGBoost
-
-
 import pytest
 from streamlit.testing.v1 import AppTest
 
@@ -19,13 +10,7 @@ from helix.options.file_paths import (
     ml_predictions_path,
 )
 
-from .fixtures import (
-    data_opts,
-    dummy_data,
-    execution_opts,
-    new_experiment,
-    plotting_opts,
-)
+from .fixtures import new_experiment
 
 
 def test_page_loads_without_exception():
