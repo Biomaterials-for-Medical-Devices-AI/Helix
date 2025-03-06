4bfa6627e4f3274c03ec50059b1563d056a5c599
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-03-06 19:12:02 +0000

    test majority vote

--- tests/pages/test_5_Feature_Importance.py
+++ tests/pages/test_5_Feature_Importance.py
@@ -257,3 +257,38 @@ def test_ensemble_mean(new_experiment: str, models_to_evaluate: None):
     assert list(
         filter(lambda x: x.endswith(".csv"), map(str, fi_results.iterdir()))
     )  # directory is not empty
+
+
+# TODO: rename once global and ensemble nomenclature sorted
+def test_ensemble_majority_vote(new_experiment: str, models_to_evaluate: None):
+    # Arrange
+    fi_plots = fi_plot_dir(helix_experiments_base_dir() / new_experiment)
+    fi_results = fi_result_dir(helix_experiments_base_dir() / new_experiment)
+    at = AppTest.from_file("helix/pages/5_Feature_Importance.py", default_timeout=60.0)
+    at.run()
+
+    # Act
+    # Select the experiment
+    at.selectbox[0].select(new_experiment).run()
+    # Select explain all models
+    at.toggle[0].set_value(True).run()
+    # Select permutation importance; global method required for ensemble
+    at.checkbox[0].check().run()
+    # Select ensemble majority vote
+    at.checkbox[3].check().run()
+    # Leave additional configs as the defaults
+    # Leave save output toggles as true, the default
+    # Run
+    at.button[0].click().run()
+
+    # Assert
+    assert not at.exception
+    assert not at.error
+    assert fi_plots.exists()
+    assert list(
+        filter(lambda x: x.endswith(".png"), map(str, fi_plots.iterdir()))
+    )  # directory is not empty
+    assert fi_results.exists()
+    assert list(
+        filter(lambda x: x.endswith(".csv"), map(str, fi_results.iterdir()))
+    )  # directory is not empty
