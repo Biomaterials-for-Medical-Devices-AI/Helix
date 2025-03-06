fca6e8eabd277b754e0a7c8c90681de3f8e8cb1a
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-03-06 16:37:16 +0000

    test warning

--- tests/pages/test_5_Feature_Importance.py
+++ tests/pages/test_5_Feature_Importance.py
@@ -94,3 +94,25 @@ def test_page_can_find_models(new_experiment: str, models_to_evaluate: None):
     assert not at.exception
     assert not at.error
     assert not at.info  # the info box saying there's no models shouldn't appear
+
+
+def test_warning_appears_with_no_global_methods_selected(
+    new_experiment: str, models_to_evaluate: None
+):
+    # Arrange
+    at = AppTest.from_file("helix/pages/5_Feature_Importance.py", default_timeout=60.0)
+    at.run()
+
+    # Act
+    # Select the experiment
+    at.selectbox[0].select(new_experiment).run()
+    # Select explain all models
+    at.toggle[0].set_value(True).run()
+
+    # Assert
+    assert not at.exception
+    assert not at.error
+    assert (
+        at.warning[0].value
+        == "You must configure at least one global feature importance method to perform ensemble methods."
+    )
