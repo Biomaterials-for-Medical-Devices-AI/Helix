0b412ccb5ddcf5de068dd93bb3e0ab78746e47a2
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-03-06 16:18:04 +0000

    fixture of models

--- tests/pages/test_5_Feature_Importance.py
+++ tests/pages/test_5_Feature_Importance.py
@@ -6,8 +6,18 @@
 # 6. Test for results
 
 
+from pickle import dump
+import numpy as np
 import pytest
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from streamlit.testing.v1 import AppTest
+
+from helix.options.data import DataOptions, DataSplitOptions
+from helix.options.enums import DataSplitMethods
+from helix.options.execution import ExecutionOptions
+from helix.options.file_paths import helix_experiments_base_dir, ml_model_dir
+from helix.services.ml_models import save_model
 
 from .fixtures import (
     data_opts,
@@ -18,12 +28,36 @@ from .fixtures import (
 )
 
 
+@pytest.fixture
+def models_to_evaluate(
+    new_experiment: str,
+    dummy_data: np.ndarray,
+    execution_opts: ExecutionOptions,
+    data_opts: DataOptions,
+):
+    save_dir = ml_model_dir(helix_experiments_base_dir() / new_experiment)
+    data_opts.data_split = DataSplitOptions(
+        n_bootstraps=3, method=DataSplitMethods.Holdout
+    )
+    X, y = dummy_data[:, :-1], dummy_data[:, -1]
+    for i in range(data_opts.data_split.n_bootstraps):
+        X_train, _, y_train, _ = train_test_split(
+            X,
+            y,
+            test_size=data_opts.data_split.test_size,
+            random_state=execution_opts.random_state + i,
+        )
+        model = LogisticRegression()
+        model.fit(X_train, y_train)
+        save_model(model, save_dir / f"{model.__class__.__name__}-{i}.pkl")
+
+
 def test_page_loads_without_exception():
     # Arrange
-    at = AppTest.from_file("helix/pages/5_Feature_Importance.py")
+    at = AppTest.from_file("helix/pages/5_Feature_Importance.py", default_timeout=60.0)
 
     # Act
-    at.run(timeout=10.0)
+    at.run()
 
     # Assert
     assert not at.exception
@@ -32,8 +66,8 @@ def test_page_loads_without_exception():
 
 def test_page_can_find_experiment(new_experiment: str):
     # Arrange
-    at = AppTest.from_file("helix/pages/5_Feature_Importance.py")
-    at.run(timeout=10.0)
+    at = AppTest.from_file("helix/pages/5_Feature_Importance.py", default_timeout=60.0)
+    at.run()
 
     # Act
     at.selectbox[0].select(new_experiment).run()
@@ -41,6 +75,22 @@ def test_page_can_find_experiment(new_experiment: str):
     # Assert
     assert not at.exception
     assert not at.error
+    assert at.info  # the info saying there's no models should appear
     with pytest.raises(ValueError):
         # check for error for non existent experiment
         at.selectbox[0].select("non-existent").run()
+
+
+def test_page_can_find_models(new_experiment: str, models_to_evaluate: None):
+    # Arrange
+    at = AppTest.from_file("helix/pages/5_Feature_Importance.py", default_timeout=60.0)
+    at.run()
+
+    # Act
+    # Select the experiment
+    at.selectbox[0].select(new_experiment).run()
+
+    # Assert
+    assert not at.exception
+    assert not at.error
+    assert not at.info  # the info box saying there's no models shouldn't appear
