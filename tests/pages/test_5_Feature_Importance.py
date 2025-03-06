8b1afae32339581db0d99744dc2661111ca160a3
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-03-06 16:43:07 +0000

    test fuzzy unavailable

--- tests/pages/test_5_Feature_Importance.py
+++ tests/pages/test_5_Feature_Importance.py
@@ -120,3 +120,28 @@ def test_ensemble_methods_disabled_without_global(
     # check these methods are disabled
     assert at.checkbox[2].disabled
     assert at.checkbox[3].disabled
+
+
+# TODO: rename once global and ensemble nomenclature sorted
+def test_fuzzy_unavailable_without_ensemble_and_local_methods(
+    new_experiment: str, models_to_evaluate: None
+):
+    # Arrange
+    at = AppTest.from_file("helix/pages/5_Feature_Importance.py", default_timeout=60.0)
+    at.run()
+
+    # Act
+    # Select the experiment
+    at.selectbox[0].select(new_experiment).run()
+    # Select explain all models
+    at.toggle[0].set_value(True).run()
+
+    # Assert
+    assert not at.exception
+    assert not at.error
+    assert (
+        at.warning[1].value
+        == "You must configure both ensemble and local importance methods to use fuzzy feature selection."
+    )
+    # check these methods are disabled
+    assert at.checkbox[6].disabled
