00f36b6bb4971423a20ac0d8e7cfaa63e82af612
Grazziela <61280906+Grazziela@users.noreply.github.com>
2025-03-11 10:12:41 +0000

    Changed order of plots to match table results order (#12)
    * Changed order of plots to match table results order
    
    * Added package dependencies
    
    * regenerate lock file
    
    * fix flake8 error
    
    * put back missing existance check
    
    ---------
    
    Co-authored-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- helix/components/plots.py
+++ helix/components/plots.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 
 @st.experimental_fragment
@@ -14,7 +15,7 @@ def plot_box(plot_dir: Path, box_title: str):
         box_title (str): The title of the plot box.
     """
     if plot_dir.exists():
-        plots = list(plot_dir.iterdir())
+        plots = sorted(plot_dir.iterdir(), key=lambda x: x.stat().st_ctime)
         with st.expander(box_title, expanded=len(plots) > 0):
             for p in plots:
                 if p.name.endswith(".png"):
@@ -30,39 +31,67 @@ def display_metrics_table(metrics_path: Path):
         metrics_path (Path): The path to the metrics file.
     """
     # Load the metrics from the file
-    if metrics_path.exists():
-        with open(metrics_path, "r") as f:
-            metrics_dict = json.load(f)
+    with open(metrics_path, "r") as f:
+        metrics_dict = json.load(f)
 
-        # Prepare data for the table
-        rows = []
-        for algorithm, results in metrics_dict.items():
-            for dataset, metrics in results.items():
-                for metric, values in metrics.items():
-                    row = {
-                        "Model": algorithm,
-                        "Set": dataset.capitalize(),
-                        "Metric": metric,
-                        "Mean ± Std": f"{values['mean']:.3f} ± {values['std']:.3f}",
-                    }
-                    rows.append(row)
+    # Prepare data for the table
+    rows = []
+    for algorithm, results in metrics_dict.items():
+        for dataset, metrics in results.items():
+            for metric, values in metrics.items():
+                row = {
+                    "Model": algorithm,
+                    "Set": dataset.capitalize(),
+                    "Metric": metric,
+                    "Mean ± Std": f"{values['mean']:.3f} ± {values['std']:.3f}",
+                }
+                rows.append(row)
 
-        # Create a DataFrame
-        df = pd.DataFrame(rows)
+    # Create a DataFrame
+    df = pd.DataFrame(rows)
 
-        # Pivot the DataFrame for a cleaner table
-        table = df.pivot(
-            index=["Model", "Set"], columns="Metric", values="Mean ± Std"
-        ).reset_index()
-        table = table.set_index("Model")
-        # Display the table in Streamlit
-        st.write("### Metrics Summary")
-        st.write(
-            "Metrics are the mean (± standard deviation) of all bootstraps (if using the Holdout"
-            " data split) or cross-validation folds (if using K-fold data split or"
-            " automatic hyper-parameter search)."
-        )
-        st.dataframe(table, use_container_width=True)
+    # Pivot the DataFrame for a cleaner table
+    table = df.pivot(
+        index=["Model", "Set"], columns="Metric", values="Mean ± Std"
+    ).reset_index()
+    table = table.set_index("Model")
+    table.sort_values(["Model", "Set"], ascending=[True, True], inplace=True)
+
+    # Display the table in Streamlit
+    st.write("### Metrics Summary")
+    st.write(
+        "Metrics are the mean (± standard deviation) of all bootstraps (if using the Holdout"
+        " data split) or cross-validation folds (if using K-fold data split or"
+        " automatic hyper-parameter search)."
+    )
+    # TODO: This can be moved to a separate function
+    # Build Grid Options
+    table = table.reset_index()
+    gb = GridOptionsBuilder.from_dataframe(table)
+
+    # Apply Global Font Styling to All Columns
+    global_style = {
+        "fontSize": "14px",
+        "fontFamily": "Arial, sans-serif",
+        "color": "black",
+        "textAlign": "center",
+    }
+
+    gb.configure_default_column(
+        editable=False, resizable=True, flex=2, cellStyle=global_style, wrapText=False
+    )
+    gb.configure_grid_options(domLayout="autoHeight")
+    gb.configure_first_column_as_index()
+    grid_options = gb.build()
+
+    # Display Full-Width Ag-Grid Table with Styling
+    AgGrid(
+        table,
+        gridOptions=grid_options,
+        fit_columns_on_grid_load=True,
+        highlight_odd_rows=True,
+        key="metrics_table",
+    )
 
 
 @st.experimental_fragment
