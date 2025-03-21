5f8c2a85a365e268828aac186055599dedde18d5
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-03-21 12:14:12 +0000

    move some code around

--- helix/machine_learning/models/brnn.py
+++ helix/machine_learning/models/brnn.py
@@ -176,7 +176,7 @@ class BaseBRNN(nn.Module):
                 outputs = self(batch_X)
 
                 # Compute total loss
-                loss = compute_brnn_loss(
+                loss = self.compute_brnn_loss(
                     self, outputs, batch_y, self._brnn_options, problem_type
                 )
                 loss.backward()
@@ -221,6 +221,56 @@ class BaseBRNN(nn.Module):
 
         return all_params, trainable_params
 
+    def _bayesian_regularization_loss(self) -> torch.Tensor:
+        """
+        Compute the Bayesian Regularization loss.
+
+        The loss is computed as the sum of squared differences
+        between model parameters and their prior mean,
+        scaled by the prior standard deviation.
+
+        Returns:
+            torch.Tensor: The computed regularization loss.
+        """
+        # Calculate regularization loss
+        reg_loss = 0.0
+        for param in self.parameters():
+            reg_loss += torch.sum((param - self.prior_mu) ** 2) / (
+                2 * self.prior_sigma**2
+            )
+        return reg_loss
+
+    def compute_brnn_loss(
+        self,
+        outputs: torch.Tensor,
+        targets: torch.Tensor,
+        problem_type: ProblemTypes,
+    ) -> torch.Tensor:
+        """
+        Compute the total loss based on the problem type
+        and include regularization loss.
+
+        Args:
+            outputs (torch.Tensor): The predicted outputs from the model.
+            targets (torch.Tensor): The true target values.
+            problem_type (ProblemTypes): The problem type.
+
+        Returns:
+            torch.Tensor: The total computed loss, including both
+            predictive and regularization loss.
+        """
+        # Compute predictive loss
+        predictive_loss = self._make_loss(problem_type, outputs, targets)
+
+        # Compute regularization loss
+        reg_loss = self._bayesian_regularization_loss(
+            prior_mu=self.prior_mu, prior_sigma=self.prior_sigma
+        )
+
+        # Combine both losses
+        total_loss = predictive_loss + self.lambda_reg * reg_loss
+        return total_loss
+
     def save_model(self, destination: Path):
         """
         Saves the model's state dictionary to a file.
@@ -230,73 +280,6 @@ class BaseBRNN(nn.Module):
     # define purely for help from the IDE
     def parameters(self, recurse=True):
         return super().parameters(recurse)
-
-
-def bayesian_regularization_loss(
-    model: BaseBRNN,
-    prior_mu: float = None,
-    prior_sigma: float = None,
-) -> torch.Tensor:
-    """
-    Compute the Bayesian Regularization loss.
-
-    The loss is computed as the sum of squared differences
-    between model parameters and their prior mean,
-    scaled by the prior standard deviation.
-
-    Args:
-        prior_mu (float, optional): The prior mean. Defaults
-        to `self._opt.prior_mu` if not provided.
-
-        prior_sigma (float, optional): The prior standard deviation.
-        Defaults to `self._opt.prior_sigma` if not provided.
-
-    Returns:
-        torch.Tensor: The computed regularization loss.
-
-    Raises:
-        ValueError: If both `prior_mu` and `prior_sigma` are not provided.
-    """
-    # Calculate regularization loss
-    reg_loss = 0.0
-    for param in model.parameters():
-        reg_loss += torch.sum((param - prior_mu) ** 2) / (2 * prior_sigma**2)
-    return reg_loss
-
-
-def compute_brnn_loss(
-    model: BaseBRNN,
-    outputs: torch.Tensor,
-    targets: torch.Tensor,
-    brnn_options: BrnnOptions,
-    problem_type: ProblemTypes,
-) -> torch.Tensor:
-    """
-    Compute the total loss based on the problem type
-    and include regularization loss.
-
-    Args:
-        model (nn.Module): The neural network model.
-        outputs (torch.Tensor): The predicted outputs from the model.
-        targets (torch.Tensor): The true target values.
-        brnn_options (BrnnOptions): The options for the neural network.
-        problem_type (ProblemTypes): The problem type.
-
-    Returns:
-        torch.Tensor: The total computed loss, including both
-        predictive and regularization loss.
-    """
-    # Compute predictive loss
-    predictive_loss = model._make_loss(problem_type, outputs, targets)
-
-    # Compute regularization loss
-    reg_loss = bayesian_regularization_loss(
-        model, prior_mu=brnn_options.prior_mu, prior_sigma=brnn_options.prior_sigma
-    )
-
-    # Combine both losses
-    total_loss = predictive_loss + brnn_options.lambda_reg * reg_loss
-    return total_loss
 
 
 class BayesianRegularisedNNClassifier(ClassifierMixin, BaseEstimator, BaseBRNN):
