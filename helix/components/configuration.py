0a930a78787ccc9d9bd27a5b76e1c8918384b562
Grazziela <g.figueredo@nottingham.ac.uk>
2025-03-24 23:37:05 +0000

    increased DPI

--- helix/components/configuration.py
+++ helix/components/configuration.py
@@ -61,10 +61,10 @@ def plot_options_box(plot_opts: PlottingOptions | None = None):
             value=plot_opts.height if plot_opts else 10,
         )
         dpi = st.slider(
-            "Image Resolution (DPI",
+            "Image Resolution (DPI)",
             min_value=330,
-            max_value=1000,
-            value=plot_opts.dpi if plot_opts else 150,
+            max_value=2000,
+            value=plot_opts.dpi if plot_opts else 330,
             key=PlotOptionKeys.DPI,
         )
         rotate_x = st.number_input(
