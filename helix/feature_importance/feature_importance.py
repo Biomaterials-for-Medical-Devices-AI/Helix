8e3c52f670bbe4978096536fda1ddd9e4184bfa2
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2025-04-02 13:10:47 +0100

    FI calculators

--- helix/feature_importance/feature_importance.py
+++ helix/feature_importance/feature_importance.py
@@ -1,7 +1,11 @@
+from typing import Any, Callable
+import pandas as pd
+from sklearn.base import BaseEstimator
 from helix.feature_importance.interpreter import Interpreter
 from helix.options.execution import ExecutionOptions
 from helix.options.fi import FeatureImportanceOptions
 from helix.options.plotting import PlottingOptions
+from helix.utils.logging_utils import Logger
 
 
 def run(
@@ -24,3 +28,32 @@ def run(
     )
 
     return gloabl_importance_results, local_importance_results, ensemble_results
+
+
+class BaseFeatureImportanceCalculator:
+    def __init__(
+        self,
+        func: Callable,
+        logger: Logger | None = None,
+    ):
+        self._func = func
+        self._logger = logger
+
+    def calculate(
+        self,
+        model: BaseEstimator,
+        X: pd.DataFrame,
+        y: pd.DataFrame | None = None,
+        **kwargs,
+    ) -> pd.DataFrame:
+        raise NotImplementedError
+
+
+class FeatureImportanceCalculator(BaseFeatureImportanceCalculator):
+    def calculate(self, model, X, y=None, **kwargs) -> pd.DataFrame:
+        return self._func(model, X, logger=self._logger, **kwargs)
+
+
+class ShapFeatureImportanceCalculator(BaseFeatureImportanceCalculator):
+    def calculate(self, model, X, y=None, **kwargs) -> tuple[pd.DataFrame, Any]:
+        return self._func(model, X, logger=self._logger, **kwargs)
