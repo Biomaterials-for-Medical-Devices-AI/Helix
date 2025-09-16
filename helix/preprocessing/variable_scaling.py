0388bce26edc3a31beb6def8b38e428242c7ce79
Eduardo Aguilar <earguila00@hotmail.com>
2025-09-16 13:22:41 -0600

    add ParetoScaler

--- helix/preprocessing/variable_scaling.py
+++ helix/preprocessing/variable_scaling.py
@@ -35,3 +35,37 @@ class MeanCenterPoissonScaler(BaseEstimator, TransformerMixin):
         """Reconstruct the original data."""
         X = np.asarray(X)
         return X * self.scale_ + self.mean_
+
+
+class ParetoScaler(BaseEstimator, TransformerMixin):
+    """
+    Pareto Scaling transformer.
+
+    - Scales each feature by sqrt(std).
+    - Unlike standardization, does NOT mean-center the data.
+
+    Compatible with scikit-learn pipelines.
+    """
+
+    def __init__(self):
+        self.scale_ = None
+
+    def fit(self, X, y=None):
+        """Compute Pareto scaling factor for each feature."""
+        X = self._validate_data(X, dtype=np.float64, force_all_finite="allow-nan")
+        std = np.nanstd(X, axis=0, ddof=0)
+        # Avoid division by zero in case std is 0
+        self.scale_ = np.sqrt(np.where(std == 0, 1, std))
+        return self
+
+    def transform(self, X):
+        """Apply Pareto scaling."""
+        X = self._validate_data(
+            X, dtype=np.float64, reset=False, force_all_finite="allow-nan"
+        )
+        return X / self.scale_
+
+    def inverse_transform(self, X):
+        """Reconstruct the original data."""
+        X = np.asarray(X)
+        return X * self.scale_
