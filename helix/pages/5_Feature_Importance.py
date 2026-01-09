4bf2f4a4b7856fa5b9a6c1b42e873e803a7c2df4
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2026-01-09 10:37:41 +0000

    remove print statements

--- helix/pages/5_Feature_Importance.py
+++ helix/pages/5_Feature_Importance.py
@@ -277,8 +277,6 @@ def display_fuzzy_plots(experiment_dir: Path) -> None:
     if fuzzy_impacts.exists():
         impacts_df = pd.read_csv(fuzzy_impacts)
         impacts_df.rename({"Unnamed: 0": ""}, axis=1, inplace=True)
-        print(impacts_df)
-        print(impacts_df.columns)
         fuzzy_feature_impacts(impacts_df)
 
 
