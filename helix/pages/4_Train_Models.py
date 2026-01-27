fe1cc2fc40d24752712cc5a1026beb4a0b3e157e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2026-01-27 16:02:27 +0000

    add the id column back

--- helix/pages/4_Train_Models.py
+++ helix/pages/4_Train_Models.py
@@ -167,9 +167,9 @@ def pipeline(
                     "Bootstrap": i,
                 }
                 df_test = pd.DataFrame(predictions_test)
-                predictions = pd.concat(
-                    [predictions, df_train, df_test], ignore_index=True
-                )
+                df_whole = pd.concat([df_train, df_test], ignore_index=True)
+                df_whole.insert(loc=0, column=data.id_column.name, value=data.id_column)
+                predictions = pd.concat([predictions, df_whole], ignore_index=True)
         st.session_state[MachineLearningStateKeys.Predictions] = predictions
 
     if ml_opts.use_hyperparam_search:
