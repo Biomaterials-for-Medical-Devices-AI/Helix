46721c2c5d3cea9fa106554591f710348412c194
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2026-02-10 14:20:02 +0000

    clarify local fi df building

--- helix/feature_importance/fuzzy.py
+++ helix/feature_importance/fuzzy.py
@@ -78,16 +78,17 @@ class Fuzzy:
             X_train = self._fuzzy_granularity(X_train)
             X_test = self._fuzzy_granularity(X_test)
 
-        # Step 3.1: Convert local FI results to a dataframe
-        master_importance_df = pd.DataFrame()
-        for _, model_local_fi_df in local_results.items():
-            master_importance_df = pd.concat(
-                [master_importance_df, model_local_fi_df], axis=0
-            )
-        master_importance_df.reset_index(drop=True, inplace=True)
+        # Step 3.1: Convert local FI results to a dataframe.
+        # Local FI results come as a dict[str, DataFrame] where the keys are the
+        # model names and the values are data frames of local FI data.
+        # NB: axis=0 stacks the rows of the data frames and ignore_index=True
+        # prevents repeated values in the row index.
+        local_importance_df = pd.concat(
+            [df for df in local_results.values()], axis=0, ignore_index=True
+        )
 
         # Step 3.2: Extract fuzzy rules from master dataframe
-        fuzzy_rules_df = self._fuzzy_rule_extraction(master_importance_df)
+        fuzzy_rules_df = self._fuzzy_rule_extraction(local_importance_df)
         save_importance_results(
             feature_importance_df=fuzzy_rules_df,
             model_type=None,
