51068cd1e8fc5385c8ff352563887038ec856109
Amelia Downs <ameliadowns007@outlook.com>
2026-08-11 15:57:32 +0100

    Reformatting using isort and black

--- helix/services/plotting.py
+++ helix/services/plotting.py
@@ -14,6 +14,7 @@ from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay
 
 from helix.options.plotting import PlottingOptions
 
+
 def plot_target_variable_distribution(
     data: pd.DataFrame,
     show_kde: bool,
