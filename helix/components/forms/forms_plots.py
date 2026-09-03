2dcd67205efca7b0f5ea7009039a5b5625ccab00
Amelia Downs <ameliadowns007@outlook.com>
2026-09-03 15:53:54 +0100

    isort black

--- helix/components/forms/forms_plots.py
+++ helix/components/forms/forms_plots.py
@@ -4,7 +4,6 @@ from typing import List, Tuple
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import plotly.express as px
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
